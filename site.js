acc45d5c60930506994255c38b9cf761d98feb75
gianduja <jlagrange@gmail.com>
2018-02-10 23:24:31 +0100

    :art: indentation

--- site.js
+++ site.js
@@ -1,75 +1,78 @@
 var keyboards = 21;
 
-	/* ADD KEYBOARDS TO CANVAS */
+/* ADD KEYBOARDS TO CANVAS */
 
-	for (var i = 0; i < keyboards; i++) {
+for (var i = 0; i < keyboards; i++) {
 
-		$('#canvas').append('<div class="keyboard keyboard' + i + '"></div>');
+	$('#canvas').append('<div class="keyboard keyboard' + i + '"></div>');
+}
+
+
+/* KEYS AND VALUES */
+
+var keys = [
+	"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+	"q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+	"a", "s", "d", "f", "g", "h", "j", "k", "l", "ò",
+	"z", "x", "c", "v", "b", "n", "m", ",", ".", "-"
+]
+
+/* ADD KEYS TO KEYBOARDS */
+
+for (var i = 0; i < keys.length; i++) {
+	$('.keyboard').append('<div class="key key-' + keys[i] + '"><span>'+ keys[i] +'</span></div>');
+}
+
+$('.key-0, .key-p, .key-ò').after('<br/>');
+
+/* TOGGLE WHITE ON PRESS */
+
+$(document).keydown(function(e){
+	
+	if (keys.indexOf(e.key) > -1) {
+		$('.active .key-' + e.key).toggleClass('on');
 	}
+	
+});
 
+/* ACTIVE KEYBOARD */
 
-	/* KEYS AND VALUES */
+var activeKeyboard = 0;
+$('.keyboard' + activeKeyboard).addClass('active');
 
-	var keys = [
-		"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-		"q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-		"a", "s", "d", "f", "g", "h", "j", "k", "l", "ò",
-		"z", "x", "c", "v", "b", "n", "m", ",", ".", "-"
-	]
+/* OTHER KEYPRESSES */
 
-	/* ADD KEYS TO KEYBOARDS */
+$(document).keydown(function(e){
 
-	for (var i = 0; i < keys.length; i++) {
-		$('.keyboard').append('<div class="key key-' + keys[i] + '"><span>'+ keys[i] +'</span></div>');
-	}
+	/* CYCLE KEYBOARDS */
 
-	$('.key-0, .key-p, .key-ò').after('<br/>');
+	if ( e.key == 'Shift' ) { 
 
-	/* TOGGLE WHITE ON PRESS */
-
-	$(document).keydown(function(e){
-		
-		if (keys.indexOf(e.key) > -1) {
-			$('.active .key-' + e.key).toggleClass('on');
+		if (activeKeyboard < keyboards-1) {
+			activeKeyboard++;
+		} else { 
+			activeKeyboard = 0 
 		}
-		
-	});
 
-	/* ACTIVE KEYBOARD */
+		$('.keyboard').removeClass('active');
+		$('.keyboard' + activeKeyboard).addClass('active');
 
-	var activeKeyboard = 0;
-	$('.keyboard' + activeKeyboard).addClass('active');
+	}
 
-	/* OTHER KEYPRESSES */
+	/* CLEAR */
 
-	$(document).keydown(function(e){
+	if ( e.key == ' ' ) {
 
-		/* CYCLE KEYBOARDS */
+		$('.key').removeClass('on'); 
 
-		if ( e.key == 'Shift' ) { 
+	}
 
-			if (activeKeyboard < keyboards-1) {
-				activeKeyboard++; } else { activeKeyboard = 0 }
+	/* SHOW/HIDE */
 
-				$('.keyboard').removeClass('active');
-				$('.keyboard' + activeKeyboard).addClass('active');
+	if ( e.key == 'Control' ) {
 
-			}
+		$('#canvas').toggleClass('show');
 
-			/* CLEAR */
+	}
 
-			if ( e.key == ' ' ) {
-
-				$('.key').removeClass('on'); 
-
-			}
-
-			/* SHOW/HIDE */
-
-			if ( e.key == 'Control' ) {
-
-				$('#canvas').toggleClass('show');
-
-			}
-
-		});
+});
